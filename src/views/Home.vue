1f7413e03df2c395477089fa57fc684cad3c0eb6
yuanxiangwei <yuanxiangwei@zhiketong.cn>
2020-12-11 10:00:40 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<van-button type="primary">主要按钮</van-button>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+	import Vue from 'vue';
+import { Button } from 'vant';
+Vue.use(Button)
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: '',
+  data () {
+    return {
+ 
+    }
+  },
+  methods:{
+	a(){
+		
+	}
+  },
+  mounted(){
+
   }
 }
 </script>
+
+<style scoped>
+
+</style>
