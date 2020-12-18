6d2cf562a50c2af437254750a2fdff21d2cb26da
yuanxiangwei <yuanxiangwei@zhiketong.cn>
2020-12-18 11:30:40 +0800

    ceshi

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,14 @@
 		<van-button type="primary">主要按钮</van-button>
 		{{url}}
 		<h1>{{u}}</h1>
+		<h2>{{user}}</h2>
 		{{code}}
 	</div>
 
 </template>
 
 <script>
+	import Cookie from 'js-cookie'
 	import Vue from 'vue';
 import { Button } from 'vant';
 Vue.use(Button)
@@ -23,13 +25,16 @@ export default {
     return {
  	url:"",
  	code:'',
- 	user
+ 	user:''
     }
   },
   created(){
   	this.url = window.location.href + window.location.url
   	this.code = window.location
-  	this.user = Cookie.get('userInfo')
+  	setTimeout(()=>{
+  		  	this.user = Cookie.get('userInfo')
+  	},5000)
+
   },
   methods:{
 	a(){
