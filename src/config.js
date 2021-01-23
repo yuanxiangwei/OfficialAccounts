7e2ecadce90ea96a900b4c830142366a70551f01
yuanxiangwei <yuanxiangwei@zhiketong.cn>
2021-01-23 19:50:48 +0800

    完成

--- src/config.js
+++ src/config.js
@@ -1,10 +1,9 @@
 export default {
 	baseUrl:'http://www.jkjyfw.cn:8080',
-	imgBaseUrl:'http://www.jkjyfw.cn:8080/syx_fy_api_module',
+	imgBaseUrl:this.baseUrl+'/syx_fy_api_module',
 	title:'疾控家园',
 	appid:'wxaae04226b4e573d0',
 	getWxUrl:function(url){
-		//let url ='https://www.qugouzhigong.top/syx_fy_api_module/weixin/callback/init'
 		return	`https://open.weixin.qq.com/connect/oauth2/authorize?appid=${this.appid}&redirect_uri=${encodeURIComponent(url)}&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect`
 	}
 }
